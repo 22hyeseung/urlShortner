0cfc1044bffd2458805aea0f8cf213b44a7906d0
huusz <irinso93@gmail.com>
2017-08-30 15:47:46 +0900

    random 스트링 추가

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const basicAuth = require('express-basic-auth')
+const randomstring = require('randomstring')
+
+// 실무에서는 데이터를 이렇게 관리하지 않는다.
+// 이렇게 만들어진 데이터는 메모리상에서 관리되는 것으로 새로고침시 사라지는 데이터이다.
+const data = [
+  {longURL: 'http://google.com', id:randomstring.generate(6)}
+]
+
+// http://localhost:3000/58DX37
+// 302 응답
 
 const app = express()
 
@@ -15,7 +25,7 @@ app.use('/static', express.static('public'))
 app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs', {data})
 })
 
 app.listen(3000, () => {
