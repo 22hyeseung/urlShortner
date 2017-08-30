eaed3b1bda4d66c64af4c37282b282fe9a586901
huusz <irinso93@gmail.com>
2017-08-30 15:26:51 +0900

    로깅 인증 설정

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+const basicAuth = require('express-basic-auth')
+
 const app = express()
+
+app.use(basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+}))
 
 app.set('view engine', 'ejs')
 app.use('/static', express.static('public'))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
